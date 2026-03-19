cd96180a7692b959843d1fb84494bb8c86189174
Jean Carlos Eleazar Morales Gutiérrez <jmoralesg@people-apps.com>
2026-03-19 01:00:36 -0600

    feat: add destination entity in domain layer

---
+++ lib/domain/entities/destination.dart
@@ -0,0 +1,21 @@
+class Destination {
+  final int id;
+  final String name;
+  final String description;
+  final String imageUrl;
+  final String category;
+  final String locationName;
+  final double latitude;
+  final double longitude;
+
+  const Destination({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+    required this.category,
+    required this.locationName,
+    required this.latitude,
+    required this.longitude,
+  });
+}
